29885bf89ff3f97ddac4629e5dcfd36012b65c86
NHN <nhn@NHNui-MacBookPro.local>
2020-08-13 08:17:44 +0900

    storybook

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
+    <select name="test" id="test" @change="routing">
+      <option value="Todo">Todo</option>
+      <option value="About">About</option>
+    </select>
     <router-view />
   </div>
 </template>
 <script>
 export default {
   name: 'App',
+  methods: {
+    routing(e) {
+      console.log(e.target.value);
+      this.$router.push({ path: e.target.value, name: e.target.value });
+    },
+  },
 };
 </script>
 <style lang="scss"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Todo from '../views/Todo';
+import About from '../views/About';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Todo',
     component: Todo,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
   },
 ];
 
@@ -19,3 +25,32 @@ const router = new VueRouter({
 });
 
 export default router;
+
+// const routesTest = [
+//   {
+//     path: '/admin',
+//     component: Admin,
+//     children: [
+//       { path: 'setting', component: AdminSetting },
+//       { path: 'manage', component: AdminManage },
+//       { path: 'info', component: AdminInfo },
+//       { path: 'memstatus', component: AdminMemStatus },
+//       { path: 'groupstatus', component: AdminMemStatus },
+//     ],
+//   },
+//   {
+//     path: '/group',
+//     component: Group,
+//     children: [
+//       { path: 'setting', component: GroupSetting },
+//       { path: 'manage', component: GroupManage },
+//       { path: 'info', component: GroupInfo },
+//       { path: 'memstatus', component: GroupMemStatus },
+//     ],
+//   },
+//   {
+//     path: '/user',
+//     component: User,
+//     children: [{ path: 'info', component: UserInfo }],
+//   },
+// ];
